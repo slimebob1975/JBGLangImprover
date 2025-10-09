050e2c3fdb74da09d00f2b199d44f48f1fd0735d
Robert Granat <robert.granat@iaf.se>
2025-10-09 11:43:28 +0200

    Updated start script

--- startup.sh
+++ startup.sh
@@ -4,10 +4,10 @@
 echo "[INFO] No known missing Linux packages..."
 
 # Starta appen
-echo "[INFO] Starting JBGtranscription API via Gunicorn"
+echo "[INFO] Starting JBGLangImrprover API via Gunicorn"
 
 # Kör med 4 workers och uvicorn workers
-exec gunicorn main:app \
+exec gunicorn app.main:app \
     --workers 2 \
     --worker-class uvicorn.workers.UvicornWorker \
     --bind 0.0.0.0:8000 \
